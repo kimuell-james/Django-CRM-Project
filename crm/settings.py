b3a0ad011d69b7d77835a492097116e60395d99a
kimuell-james <114352942+kimuell-james@users.noreply.github.com>
2025-07-21 22:34:13 +0800

    Connect postgresql database

--- crm/settings.py
+++ crm/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'accounts',
+    'accounts.apps.AccountsConfig',
     'django_filters',
 ]
 
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DEMO_TEST',
+        'USER': 'postgres',
+        'PASSWORD': 'iamadmin1130',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
